b34df7f6098fbd471b4590a7b6a91cde1a869634
ofir.oron <ofir.oron@toluna.com>
2019-05-15 15:10:12 +0300

    Use react-native-webview@^5.8.2/WebView instead of deprecated react-native/WebView

--- react-native-autoreheight-webview/AutoResizeHeightWebView.js
+++ react-native-autoreheight-webview/AutoResizeHeightWebView.js
@@ -3,7 +3,8 @@
  */
 
 import React, { Component } from 'react';
-import {StyleSheet, View, WebView , Animated} from 'react-native';
+import {StyleSheet, View , Animated} from 'react-native';
+import {WebView} from "react-native-webview";
 
 //modify webview error:  https://github.com/facebook/react-native/issues/10865
 const patchPostMessageJsCode = `
